175c4e8ca17ddbe933d0fb022111c188f942f5d7
Pranshu Kandoi <kandoipranshu@gmail.com>
2021-07-17 10:04:25 +0530

    upload files

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  //const XylophoneApp({Key key}) : super(key: key);
+
+  void playSound(int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({required Color color,required int soundNumber}){
+    return Expanded(
+      // ignore: deprecated_member_use
+      child: FlatButton(
+        onPressed: (){
+          playSound(soundNumber);
+        },
+        color: color,
+        child: Text(''),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red,soundNumber: 1),
+              buildKey(color: Colors.orange,soundNumber: 2),
+              buildKey(color: Colors.yellow,soundNumber: 3),
+              buildKey(color: Colors.green,soundNumber: 4),
+              buildKey(color: Colors.teal,soundNumber: 5),
+              buildKey(color: Colors.blue,soundNumber: 6),
+              buildKey(color: Colors.purple,soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
